be2d383a12b36e5c3ebd0e857a4b3cbbeb4f99d6
Sydney Ainsworth <sydainsworth@gmail.com>
2026-04-05 17:03:14 -0700

    reworking cipher generation

--- JS/index.js
+++ JS/index.js
@@ -23,26 +23,71 @@ function makeMatrix() {
     square[2][2] = "0";
 }
 
+function sanKey(key) {
+    let seen = new Set();
+    let result = '';
+
+    for (let char of key){
+        if (!seen.has(char)){
+            result += char;
+            seen.add(char);
+        }
+    }
+
+    return result;
+}
+
 function genCipher() {
     keyword = document.getElementById("keyword").value;
-    makeMatrix();
-
+    keyword = keyword.toLowerCase();
+    // makeMatrix();
+    // console.log(keyword);
+    keyword = sanKey(keyword);
+    // console.log(keyword);
     keyLen = keyword.length;
     // console.log("Input received:", keyLen);
 
+    var fullRows = Math.floor(keyLen / 5);
+    // console.log(fullRows);
+    var partialRow = keyLen % 5;
+    // console.log(partialRow);
+
+    var cipher = [[1,''],[2,''],[3,''],[4,''],[5,''],[8,''],[9,''],[10,''],[11,''],[6,''],[7,''],[12,''],[0,''], [12,''],[7,''],[6,''],[11,''],[10,''],[9,''],[8,''],[5,''],[4,''],[3,''],[2,''],[1,''],];
+    // console.log("(", cipher[0[0]]+", " + cipher[0[1]] + ")");
+    // console.log(cipher[0][0]); //returns 1
+    // console.log(cipher[0][1]); //correctly returns value of second element of tuple
+
+    for (let i=0; i<keyword.length; i++){
+        cipher[i][1] = keyword[i];
+    }
+    // console.log(cipher);
+
+    let alpha = new Set(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
+
+    for(let char in keyword){
+        alpha.delete(keyword[char]);
+    }
+    // console.log(alpha);
+
+    alpha = Array.from(alpha);
+    let letterOrd = keyword;
+    for (let el in alpha){
+        letterOrd += alpha[el];
+    }
+    // console.log(letterOrd);
 
 
 
 
-    //show cipher table
-    if (cipherTable.style.display === "none") {
+
+    if (cipherTable.style.display == "none") {
         cipherTable.style.display = "block";
     } else {
-        cipherTable.style.display = "none";
+        if (keyword = '') cipherTable.style.display = "none";
     }
 
     //show nav change button
-    if (toEncrypt.style.display === "none") {
+    if (toEncrypt.style.display == "none") {
         toEncrypt.style.display = "block";
     } else {
         toEncrypt.style.display = "none";
