26cde11abb0ea89944ecd9d4449c777d2bfe7824
5QU1D <sydainsworth@gmail.com>
2026-05-12 15:35:08 -0700

    restructuring to avoid passing JSON of array from one page to another

--- JS/index.js
+++ JS/index.js
@@ -50,5 +50,29 @@ function genCipher() {
 }
 
 function navChangeE() {
-    location.replace("encrypt.html")
+    // location.replace("encrypt.html")
+    input.style.display = "none";
+    cipherTable.style.display = "none";
+    toEncrypt.style.display = "none";
+
+    input2.style.display = "block";
+    output2.style.display = "block";
 }
+
+  function readText() {
+    // 1. Get the value from the text box
+    input = document.getElementById("plaintext").value;
+    
+    // 2. Perform an action with it (e.g., log to console)
+    console.log("Input received:", text);
+  }
+
+  function printText() {
+    
+  }
+
+
+  function encrypt() {
+    readText();
+
+  }
